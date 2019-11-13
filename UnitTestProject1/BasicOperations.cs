12a8c135f519298a4587b274bc5989568ed93a59
z003uspy <mete.bahadir@siemens.com>
2019-11-13 18:36:58 +0300

    First Draft

---
+++ UnitTestProject1/BasicOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace KicadAutomatedGUITests
+{
+    public class BasicOperations
+    {
+        public void Open_PCB_File(WindowsDriver<WindowsElement> testSession, string filePath, string fileName)
+        {
+            //Get the File Menu Element
+            WindowsElement element;
+            element = testSession.FindElementByName("File");
+
+            //Thread.Sleeps look ugly
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            element.SendKeys(Keys.ArrowDown);
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            element.SendKeys(Keys.Enter);
+
+            //Browse Files Dialog
+            WindowsElement browseFilesElement = testSession.FindElementByName("Open Board File");
+            browseFilesElement.SendKeys(Keys.Alt + "n");
+            browseFilesElement.SendKeys(filePath + fileName);
+
+            //Press the Open button
+            WindowsElement OpenButton = testSession.FindElementByName("Open");
+            OpenButton.Click();
+        }
+    }
+}
---
+++ UnitTestProject1/PCBNEWTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using KicadAutomatedGUITests;
+
+namespace PCBNEWTest
+{
+    [TestFixture]
+    public class OpenPCBTest : PCBNEWTestBase
+    {
+        //Move these variables to test data configuration part
+        BasicOperations helper = new BasicOperations();
+
+        string projectPath = "D:\\Projects\\";
+
+        static object[] testProjects =
+        {
+            "pcb_001.kicad_pcb",
+            "pcb_002.kicad_pcb"
+        };
+
+        [TestFixtureSetUp]
+        public static void Init()
+        {
+            Setup();
+        }
+
+        [TestFixtureTearDown]
+        public static void TearDown()
+        {
+            BaseTearDown();
+        }
+
+        [Test, TestCaseSource("testProjects")]
+        public void Open_PCBNEW_File (string pcbName)
+        {
+            string sanitizedProjectPath = SanitizeBackslashes(projectPath);
+            helper.Open_PCB_File(session, sanitizedProjectPath, pcbName);
+            Assert.AreEqual(session.Title, @"D:\Project\" + pcbName);
+        }
+
+        [Test, TestCaseSource("testProjects")]
+        public void Draw_a_Trace_In_Openned_PCB(string pcbName)
+        {
+            Assert.AreEqual("True", "True");
+        }
+
+        [Test]
+        public void Save_and_Close_The_Modified_PCB()
+        {
+            Assert.AreNotEqual(string.Empty, "Full");
+        }
+
+    }
+}
